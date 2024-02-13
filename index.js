48f088de61d1d41e6f205a7d2e550ab873659cdd
Kaio Soledade <soledadekaio@gmail.com>
2024-02-13 18:00:33 -0300

    Bot de whatsapp

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+const qrcode = require('qrcode-terminal');
+
 const { Client } = require('whatsapp-web.js');
 const client = new Client();
 
 client.on('qr', (qr) => {
-    console.log('QR RECEIVED', qr);
+    qrcode.generate(qr, { small: true }); //Cria QR code
 });
 
 client.on('ready', () => {
-    console.log('Client is ready!');
+    console.log('Client is ready!'); // Somente avisa que esta funcinando
 });
+
+client.on('message', async (message) => {
+	if (message.body === '!ping') {
+		await message.reply('pong');
+	}
+}); //Observando as mensagens do WPP
+// Se eu escrever "Ping", ele responderar "Pong"
+
 
 client.initialize();
