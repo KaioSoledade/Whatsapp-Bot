7ec8ef0227069b40cad96a4baed9f89963b5cc54
Kaio Soledade <soledadekaio@gmail.com>
2024-02-14 20:55:17 -0300

    Testando, mas não funcionou

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
 const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+// Salva sua conversa, não perdendo as conversas antigas
 
 client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true }); //Cria QR code
+    qrcode.generate(qr, { small: true }); 
 });
+//Cria QR code
 
 client.on('ready', () => {
-    console.log('Client is ready!'); // Somente avisa que esta funcinando
+    console.log('Client is ready!'); 
 });
+// Somente avisa que esta funcinando
+
 
 client.on('message', async (message) => {
 	if (message.body === '!ping') {
 		await message.reply('pong');
 	}
-}); //Observando as mensagens do WPP
+}); 
+//Observando as mensagens do WPP
 // Se eu escrever "Ping", ele responderar "Pong"
 
 
